ee0b80ec1c9214cb5a84333135b09c57c2d73134
Jörgen Hägglund <jorgenhagglund@netscape.net>
2025-08-15 15:41:23 +0200

    Parts of the CourseService completed

--- Domain.Models/Entities/Course.cs
+++ Domain.Models/Entities/Course.cs
@@ -2,7 +2,7 @@
 
 namespace Domain.Models.Entities;
 
-public class Cource : ILinkBase
+public class Course : ILinkBase
 {
     public int Id { get; set; }
     public string Name { get; set; }
---
+++ LMS.Services/CourseService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+
+namespace LMS.Services;
+
+public class CourseService(IMapper mapper)
+{
+    public Task<IEnumerable<CourseDto>> GetAllCoursesAsync()
+    {
+        // Implementation to get all courses
+        throw new NotImplementedException();
+    }
+
+    public Task<CourseDto> GetCourseByIdAsync(int courseId)
+    {
+        // Implementation to get a course by ID
+        throw new NotImplementedException();
+    }
+
+    public Task<CourseDto> GetCourseForUserAsync(string userId)
+    {
+        // Implementation to create a new course
+        throw new NotImplementedException();
+    }
+}
