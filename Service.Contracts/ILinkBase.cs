3f99f4eab799f025b40318461a2da441860aad27
Jörgen Hägglund <jorgenhagglund@netscape.net>
2025-08-15 14:56:57 +0200

    Skapade ILinkBase som grund för entities

---
+++ Service.Contracts/ILinkBase.cs
@@ -0,0 +1,10 @@
+﻿namespace Service.Contracts;
+
+public interface ILinkBase
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime Starts { get; set; }
+    public DateTime Ends { get; set; }  
+}
