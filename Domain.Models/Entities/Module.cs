cb7a08cfd4360f1ff592d4484ed8a342172af963
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-15 16:37:06 +0200

    feat(Module): Add Course and Activity relations to model

--- Domain.Models/Entities/Module.cs
+++ Domain.Models/Entities/Module.cs
@@ -1,17 +1,24 @@
 ﻿using Service.Contracts;
 using System;
 using System.Collections.Generic;
+//using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Models.Entities;
 
-internal class Module : ILinkBase
+public class Module : ILinkBase
 {
     public int Id { get; set ; }
     public string Name { get; set; }
     public string Description { get; set; }
     public DateTime Starts { get; set; }
     public DateTime Ends { get; set; }
+
+    public Course course { get; set; }
+    public int CourseId { get; set; }
+
+    public ICollection<Activity> activities { get; set; }
+   
 }
