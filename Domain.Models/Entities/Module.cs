f14bf7fb614ca7d3ef73020672b9b469cedfd173
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-15 15:13:20 +0200

    feat(Module): Implement Module model and inherit from ILinkBase

---
+++ Domain.Models/Entities/Module.cs
@@ -0,0 +1,17 @@
+﻿using Service.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Entities;
+
+internal class Module : ILinkBase
+{
+    public int Id { get; set ; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public DateTime Starts { get; set; }
+    public DateTime Ends { get; set; }
+}
