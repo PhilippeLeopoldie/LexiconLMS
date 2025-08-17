b4e83cb67a3166d2147d9cf30203c1165a5a7cea
Iryna <iryna.chorna@hotmail.com>
2025-08-17 12:19:44 +0200

    Added a Activities and ActivityTypes tables Seeding ActivityTypes to database Added Migration

--- Domain.Models/Entities/Activity.cs
+++ Domain.Models/Entities/Activity.cs
@@ -6,12 +6,6 @@ public class Activity : BaseModel
     public required DateTime StartsAt { get; set; }
     public required DateTime EndsAt { get; set; }
     public required ActivityType Type { get; set; }
+    public int ActivityTypeId { get; set; }
 
-    //// Foreign Key
-    //public int ModuleId { get; set; }
-    //// Navigation property
-    //public Module Module { get; set; }
-
-    //// Collection for documents
-    //public ICollection<Document> Documents { get; set; }
 }
