eca35090403c00a72ded279f9824d42a7cc87851
Jörgen Hägglund <jorgenhagglund@netscape.net>
2025-08-18 09:58:45 +0200

    Removed CourseService.cs. Added ICourseRepository.cs Finalized Course class

--- Domain.Models/Entities/Course.cs
+++ Domain.Models/Entities/Course.cs
@@ -1,9 +1,11 @@
-﻿using Service.Contracts;
-
-namespace Domain.Models.Entities;
+﻿namespace Domain.Models.Entities;
 
 public class Course : BaseModel
 {
+    public class Module : BaseModel { }  // TODO: Remove when actual Module class is defined
     public DateTime Starts { get; set; }
     public DateTime Ends { get; set; }
+    public string StudentId { get; set; } = null!;
+    public ApplicationUser Student { get; set; } = null!;
+    public IEnumerable<Module> Modules { get; set; } = [];
 }
