9f89c8c10752077b89fd992e0a5c00d2e2dd3921
Jörgen Hägglund <jorgenhagglund@netscape.net>
2025-08-18 09:22:57 +0200

    Rebased Course to BaseModel

--- Domain.Models/Entities/Course.cs
+++ Domain.Models/Entities/Course.cs
@@ -2,11 +2,8 @@
 
 namespace Domain.Models.Entities;
 
-public class Course : ILinkBase
+public class Course : BaseModel
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
     public DateTime Starts { get; set; }
     public DateTime Ends { get; set; }
 }
