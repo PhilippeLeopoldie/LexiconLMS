445b184e9454b01894c2b69e6c75c582332321c3
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-18 10:12:15 +0200

    feat(Module) : Inherite from BaseModel

--- Domain.Models/Entities/Module.cs
+++ Domain.Models/Entities/Module.cs
@@ -8,11 +8,8 @@ using System.Threading.Tasks;
 
 namespace Domain.Models.Entities;
 
-public class Module : ILinkBase
+public class Module : BaseModel
 {
-    public int Id { get; set ; }
-    public string Name { get; set; }
-    public string Description { get; set; }
     public DateTime Starts { get; set; }
     public DateTime Ends { get; set; }
 
