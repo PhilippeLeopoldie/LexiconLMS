15759de5b787a77348fb63fec817dbd17a638f83
Jörgen Hägglund <jorgenhagglund@netscape.net>
2025-08-18 10:16:14 +0200

    Changed Course.Modules from IEnumerable to ICollection.

--- Domain.Models/Entities/Course.cs
+++ Domain.Models/Entities/Course.cs
@@ -5,7 +5,9 @@ public class Course : BaseModel
     public class Module : BaseModel { }  // TODO: Remove when actual Module class is defined
     public DateTime Starts { get; set; }
     public DateTime Ends { get; set; }
+
     public string StudentId { get; set; } = null!;
     public ApplicationUser Student { get; set; } = null!;
-    public IEnumerable<Module> Modules { get; set; } = [];
+
+    public ICollection<Module> Modules { get; set; } = [];
 }
