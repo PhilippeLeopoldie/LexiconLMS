4020e868560da8dbb835dc8b3a43891a94c6d8f8
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-18 11:15:58 +0200

    feat(Module): Remove dependency on Service.Contracts

--- Domain.Models/Entities/Module.cs
+++ Domain.Models/Entities/Module.cs
@@ -1,7 +1,5 @@
-﻿using Service.Contracts;
-using System;
+﻿using System;
 using System.Collections.Generic;
-//using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
