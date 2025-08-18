4cae7358bc5a0aa0a37880540dc1bf30feb4bedd
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-18 18:07:16 +0200

    feat(Module): Implement Get methods in Module repository
    - Added methods in Module repository to retrieve single and multiple Module entities.
    - Supports fetching by ID and retrieving all modules by CourseId.

---
+++ LMS.Infrastructure/Repositories/ModuleRepository.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models.Entities;
+using LMS.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace LMS.Infrastructure.Repositories;
+
+public class ModuleRepository : RepositoryBase<Module>
+{
+    public ModuleRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Module?> GetModulesAsync(int courseId, bool trackChanges = false)
+    {
+        return await FindByCondition(module => module.CourseId.Equals(courseId), trackChanges).FirstOrDefaultAsync();
+    }
+
+    public async Task<Module> GetModuleByIdAsync(int id, bool includeActivities, bool trackChanges)
+    {
+        var query = FindByCondition(module => module.Id.Equals(id), trackChanges);
+
+        if (includeActivities) query = query.Include(module => module.Activities);
+        return await query.FirstOrDefaultAsync();
+    }
+
+}
