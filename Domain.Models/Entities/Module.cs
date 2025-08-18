0577f800a2653f3c977656667449ee387da3bc0d
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-18 11:21:43 +0200

    Refactor Module class property names and requirements
    Updated property names in the Module class to PascalCase
    to adhere to C# naming conventions. Changed 'course' to
    'Course' and 'activities' to 'Activities'. Marked the
    'Course' property as required to enforce its presence
    when creating a Module instance.

--- Domain.Models/Entities/Module.cs
+++ Domain.Models/Entities/Module.cs
@@ -11,9 +11,9 @@ public class Module : BaseModel
     public DateTime Starts { get; set; }
     public DateTime Ends { get; set; }
 
-    public Course course { get; set; }
+    public required Course Course { get; set; }
     public int CourseId { get; set; }
 
-    public ICollection<Activity> activities { get; set; }
+    public ICollection<Activity> Activities { get; set; }
    
 }
