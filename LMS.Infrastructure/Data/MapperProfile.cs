50c856e69f848f8bc893fc15eedd3ea68d5697ab
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-19 12:11:24 +0200

    Add module DTO mappings and update profile configurations
    - Introduced `LMS.Shared.DTOs.ModuleDtos` for module-related DTOs.
    - Implemented bidirectional mapping between `Module` and `ModuleDto`.
    - Added custom mapping for `StartsAt` in `ModuleUpdateDto` to `Module`.
    - Added custom mapping for `StartsAt` in `ModuleCreateDto` to `Module`.

--- LMS.Infrastructure/Data/MapperProfile.cs
+++ LMS.Infrastructure/Data/MapperProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Domain.Models.Entities;
 using LMS.Shared.DTOs.AuthDtos;
+using LMS.Shared.DTOs.ModuleDtos;
 
 namespace LMS.Infrastructure.Data;
 
@@ -9,5 +10,20 @@ public class MapperProfile : Profile
     public MapperProfile()
     {
         CreateMap<UserRegistrationDto, ApplicationUser>();
+        CreateMap<Module, ModuleDto>().ReverseMap();
+        CreateMap<ModuleUpdateDto, Module>()
+            .ForMember(target => target.StartsAt,
+                        options => options.MapFrom((src, destination) =>
+                                             src.StartsAt == default
+                                             ? destination.StartsAt
+                                             : src.StartsAt))
+            .ReverseMap();
+        CreateMap<ModuleCreateDto,Module>()
+            .ForMember(target => target.StartsAt,
+                        options => options.MapFrom((src, destination) =>
+                                             src.StartsAt == default
+                                             ? destination.StartsAt
+                                             : src.StartsAt))
+            .ReverseMap();
     }
 }
