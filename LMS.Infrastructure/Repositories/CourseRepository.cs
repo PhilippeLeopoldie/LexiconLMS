40c4b41e1bf213300e60c4e5a3f7a745dbbac34f
Jörgen Hägglund <jorgenhagglund@netscape.net>
2025-08-19 13:46:44 +0200

    Created CourseRepository Included CourseRepository to UnitOfWork and IUnitOfWork Added DI registration for CourseRepository Added Lazy creation for ICourseRepository

---
+++ LMS.Infrastructure/Repositories/CourseRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Contracts.Repositories;
+using Domain.Models.Entities;
+using LMS.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace LMS.Infrastructure.Repositories;
+
+public class CourseRepository(ApplicationDbContext context) : RepositoryBase<Course>(context), ICourseRepository
+{
+    public async Task<Course?> GetCourseByIdAsync(int id, bool trackChanges = false) =>
+        await FindByCondition(course => course.Id.Equals(id), trackChanges).FirstOrDefaultAsync();
+
+    public async Task<IEnumerable<Course>> GetAllCoursesAsync(bool trackChanges = false) =>
+        await FindAll(trackChanges).ToListAsync();
+}
--- LMS.Infrastructure/Repositories/UnitOfWork.cs
+++ LMS.Infrastructure/Repositories/UnitOfWork.cs
@@ -7,17 +7,20 @@ public class UnitOfWork : IUnitOfWork
     private readonly ApplicationDbContext context;
     private readonly Lazy<IActivityRepository> activityRepository;
     private readonly Lazy<IModuleRepository> moduleRepository;
+    private readonly Lazy<ICourseRepository> courseRepository;
 
 
-    public UnitOfWork(ApplicationDbContext context, Lazy<IActivityRepository> activityRepository, Lazy<IModuleRepository> moduleRepository)
+    public UnitOfWork(ApplicationDbContext context, Lazy<IActivityRepository> activityRepository, Lazy<IModuleRepository> moduleRepository, Lazy<ICourseRepository> courseRepository)
     {
         this.context = context ?? throw new ArgumentNullException(nameof(context));
         this.activityRepository = activityRepository;
         this.moduleRepository = moduleRepository;
+        this.courseRepository = courseRepository;
     }
 
     public IActivityRepository ActivityRepository => activityRepository.Value;
     public IModuleRepository ModuleRepository => moduleRepository.Value;
+    public ICourseRepository CourseRepository => courseRepository.Value;
 
     public async Task CompleteAsync() => await context.SaveChangesAsync();
 }
