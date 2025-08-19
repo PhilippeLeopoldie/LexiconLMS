822e866b6f013664362267d73c88b14187a4f800
Jörgen Hägglund <jorgenhagglund@netscape.net>
2025-08-19 16:56:16 +0200

    Creation Dto for Course created

---
+++ LMS.Shared/DTOs/CourseDtos/CourseForCreationDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Shared.DTOs.CourseDtos;
+
+public record CourseForCreationDto
+{
+    [Required(ErrorMessage = "The Name is required")]
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+    [Required(ErrorMessage = "The Start date is required")]
+    public required DateTime Starts { get; init; }
+    public required DateTime Ends { get; init; }
+}
