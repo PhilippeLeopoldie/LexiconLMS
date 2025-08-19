103dbb36c17f896978781595ca1d1a00a6c49cf5
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-19 16:01:41 +0200

    Refactor ModuleService and add GetAllModulesAsync method
    Updated the `ModuleService` class to change the private field name for the unit of work from `_unitOfWork` to `_uow`. Introduced a new asynchronous method `GetAllModulesAsync` that retrieves and maps modules to `ModuleDto` objects, returning both the DTOs and metadata.

--- LMS.Services/ModuleService.cs
+++ LMS.Services/ModuleService.cs
@@ -1,19 +1,33 @@
 ﻿using AutoMapper;
 using Domain.Contracts.Repositories;
+using LMS.Shared.Common;
+using LMS.Shared.DTOs.ModuleDtos;
 
 namespace LMS.Services;
 
 public class ModuleService
 {
-    private readonly IUnitOfWork _unitOfWork;
+    private readonly IUnitOfWork _uow;
     private readonly IMapper _mapper;
 
     public ModuleService(IUnitOfWork unitOfWork, IMapper mapper)
     {
-        _unitOfWork = unitOfWork;
+        _uow = unitOfWork;
         _mapper = mapper;
     }
 
+    public async Task<(IEnumerable<ModuleDto> moduleDtos, MetaData metaData)> GetAllModulesAsync(
+        ModuleRequestParams requestParams,
+        int courseId,
+        bool sortByName = false,
+         bool trackChanges = false
+        )
+    {
+        var pagedList = await _uow.ModuleRepository.GetModulesAsync(requestParams, courseId ,sortByName, trackChanges);
+        var ModulesDto = _mapper.Map<IEnumerable<ModuleDto>>(pagedList.Items);
+        return (ModulesDto, pagedList.MetaData);
+    }
 
     
+
 }
