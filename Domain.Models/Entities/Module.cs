4a432a7e5f4a6f0e42467273e5b6e795f4888f1c
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-19 12:10:12 +0200

    Refactor Module properties for clarity and flexibility
    Renamed properties `Starts` and `Ends` to `StartsAt` and `EndsAt` for consistency with other Entities . Changed the type of `Activities` from `ICollection<Activity>` to `IEnumerable<Activity>` to allow for more flexible, read-only access.

--- Domain.Models/Entities/Module.cs
+++ Domain.Models/Entities/Module.cs
@@ -3,12 +3,12 @@ namespace Domain.Models.Entities;
 
 public class Module: BaseModel
 {
-    public DateTime Starts { get; set; }
-    public DateTime Ends { get; set; }
+    public DateTime StartsAt { get; set; }
+    public DateTime EndsAt { get; set; }
 
     public required Course Course { get; set; }
     public int CourseId { get; set; }
 
-    public ICollection<Activity> Activities { get; set; }
+    public IEnumerable<Activity> Activities { get; set; }
 
 }
