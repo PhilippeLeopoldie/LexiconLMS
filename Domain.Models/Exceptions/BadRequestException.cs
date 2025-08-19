439f6cc4510b3a84e72b2282bab0e0583c6511a3
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-19 16:45:31 +0200

    Add BadRequestException and InvalidEntryBadRequestException
    Introduce an abstract class `BadRequestException` for handling bad request errors, featuring a `Title` property and a customizable constructor. Add `InvalidEntryBadRequestException` with two constructors for specific invalid entry scenarios.

---
+++ Domain.Models/Exceptions/BadRequestException.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Models.Exceptions;
+
+public abstract class BadRequestException : Exception
+{
+    public string Title { get; set; }
+    protected BadRequestException(string message, string title = "Bad request") : base(message)
+    {
+        Title = title;
+    }
+}
+
+public class InvalidEntryBadRequestException : BadRequestException
+{
+    public InvalidEntryBadRequestException(int id) : base($"Invalid Id: '{id}'")
+    {
+    }
+    public InvalidEntryBadRequestException() : base($"No patchDocument")
+    {
+    }
+}
