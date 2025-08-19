9efbc94fc1e241b1cbe4584ce78b1ab1b58be845
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-19 16:40:50 +0200

    Refactor NotFoundException to be abstract

--- Domain.Models/Exceptions/NotFoundException.cs
+++ Domain.Models/Exceptions/NotFoundException.cs
@@ -1,5 +1,5 @@
 ﻿namespace Domain.Models.Exceptions;
-public class NotFoundException : Exception
+public abstract class NotFoundException : Exception
 {
     public string Title { get; }
     public NotFoundException(string message, string title = "Not Found") : base(message)
