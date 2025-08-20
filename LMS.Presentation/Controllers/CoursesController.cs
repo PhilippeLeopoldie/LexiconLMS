cf15d5d39fee185402064f4259d1160f872110b2
Jörgen Hägglund <jorgenhagglund@netscape.net>
2025-08-20 15:33:38 +0200

    Updated Repository interface/class for Course Added Controller for Courses Updated Service interface/class for Course Updated ServiceManager to hold CourseService

---
+++ LMS.Presentation/Controllers/CoursesController.cs
@@ -0,0 +1,25 @@
+﻿using LMS.Shared.Common;
+using LMS.Shared.DTOs.CourseDtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Service.Contracts;
+using System.Text.Json;
+
+namespace LMS.Presentation.Controllers;
+
+[ApiController]
+[Route("api/courses")]
+[Authorize]
+public class CoursesController(IServiceManager serviceManager) : ControllerBase
+{
+    [HttpGet]
+    [Authorize(Roles = "Teacher")]
+    public async Task<ActionResult<IEnumerable<CourseDto>>> GetCoursesAsync([FromQuery] RequestParams requestParams)
+    {
+        var (courses, metaData) = await serviceManager.CourseService.GetAllCoursesAsync(requestParams);
+        Response.Headers.Append("X-Pagination", JsonSerializer.Serialize(metaData));
+        return Ok(courses);
+    }
+}
--- LMS.Services/ServiceManager.cs
+++ LMS.Services/ServiceManager.cs
@@ -5,16 +5,20 @@ namespace LMS.Services;
 public class ServiceManager : IServiceManager
 {
     private Lazy<IAuthService> authService;
+    private Lazy<ICourseService> courseService;
     private Lazy<IActivityService> activityService;
-    public IAuthService AuthService => authService.Value;
 
+    public IAuthService AuthService => authService.Value;
+    public ICourseService CourseService => courseService.Value;
     public IActivityService ActivityService => activityService.Value;
 
     public ServiceManager(
         Lazy<IAuthService> authService,
+        Lazy<ICourseService> courseService, 
         Lazy<IActivityService> activityService)
     {
         this.authService = authService;
+        this.courseService = courseService;
         this.activityService = activityService;
     }
 }
