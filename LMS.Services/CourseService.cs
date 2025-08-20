35ac6c2353d0b0125471192fc54c72956b0e1610
Jörgen Hägglund <jorgenhagglund@netscape.net>
2025-08-20 12:02:16 +0200

    Implemented GetCourseForUserAsync

--- LMS.Services/CourseService.cs
+++ LMS.Services/CourseService.cs
@@ -1,12 +1,14 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Domain.Contracts.Repositories;
+using Domain.Models.Entities;
 using LMS.Shared.DTOs.CourseDtos;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace LMS.Services;
 
-public class CourseService(IUnitOfWork unitOfWork, IMapper mapper) : ICourseService
+public class CourseService(IUnitOfWork unitOfWork, IMapper mapper, UserManager<ApplicationUser> userManager) : ICourseService
 {
     public async Task<IEnumerable<CourseDto>> GetAllCoursesAsync() =>
         await unitOfWork.CourseRepository
@@ -26,10 +28,12 @@ public class CourseService(IUnitOfWork unitOfWork, IMapper mapper) : ICourseServ
 
     public async Task<CourseDto> GetCourseForUserAsync(string userId)
     {
-        //var result = await unitOfWork.CourseRepository
-        //    .FindByCondition(c => c.Users.Any(u => u.Id == userId))
-        //    .FirstOrDefaultAsync();
-        // Implementation to create a new course
-        throw new NotImplementedException();
+        var user = await userManager.FindByIdAsync(userId);
+        if (user == null)
+            throw new ArgumentException("User not found", nameof(userId));
+        return await unitOfWork.CourseRepository
+            .FindByCondition(c => c.Id == user.CourseId)
+            .ProjectTo<CourseDto>(mapper.ConfigurationProvider)
+            .FirstOrDefaultAsync();
     }
 }
