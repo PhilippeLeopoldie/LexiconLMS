0d983325699b7c7145cb6e30d9e7cfd78bebd29b
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-21 14:43:17 +0200

    Add ModuleController for course module management
    Introduces the `ModuleController` class to handle HTTP requests for modules within a specific course. Implements routing, authorization, and Swagger documentation. Includes a method `GetModules` to retrieve all modules with pagination metadata in the response headers.

---
+++ LMS.Presentation/Controllers/ModuleController.cs
@@ -0,0 +1,46 @@
+﻿using LMS.Shared.Common;
+using LMS.Shared.DTOs.ModuleDtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LMS.Presentation.Controllers;
+
+[Route("api/courses/{courseId}:int/[controller]")]
+[ApiController]
+[Authorize]
+public class ModuleController : ControllerBase
+{
+    private readonly IServiceManager _serviceManager;
+
+    public ModuleController(IServiceManager serviceManager)
+    {
+        _serviceManager = serviceManager ??
+            throw new ArgumentNullException(nameof(serviceManager));
+    }
+
+    [HttpGet]
+    [Authorize(Roles = "Teacher, Student")]
+    [SwaggerOperation(Summary = "Get all modules", Description = "Retrieves a list of all modules for a specific course.")]
+    [SwaggerResponse(StatusCodes.Status200OK, "Activities retrieved successfully", typeof(IEnumerable<ModuleDto>))]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid request parameters")]
+    [SwaggerResponse(StatusCodes.Status401Unauthorized, "User is not authorized")]
+    [SwaggerResponse(StatusCodes.Status403Forbidden, "Access denied")]
+    public async Task<ActionResult<IEnumerable<ModuleDto>>> GetModules(int courseId, [FromQuery] ModuleRequestParams parameter)
+    {
+        var (modules, metadata) = await _serviceManager.ModuleService.GetAllModulesAsync(courseId, parameter);
+        Response.Headers.Append("X-Pagination", JsonSerializer.Serialize(metadata));
+        return Ok(modules);
+    }
+
+
+
+}
