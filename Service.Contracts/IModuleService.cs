eaf40ed15a1c235c5ac26b0b3ba8adc1f53a20d3
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-21 15:40:10 +0200

    Update IModuleService to include courseId parameter
    Modified several methods in the IModuleService interface to include an additional courseId parameter. This change allows for module operations to be performed in the context of a specific course, enhancing the interface's specificity for course-related module management.

--- Service.Contracts/IModuleService.cs
+++ Service.Contracts/IModuleService.cs
@@ -6,12 +6,12 @@ namespace Services.Contracts;
 
 public interface IModuleService
 {
-    Task DeleteModuleAsync(int id);
+    Task DeleteModuleAsync(int courseId, int id);
     Task<(IEnumerable<ModuleDto> moduleDtos, MetaData metaData)> GetAllModulesAsync(int courseId, ModuleRequestParams requestParams, bool trackChanges = false);
-    Task<ModuleDto> GetModuleByIdAsync(int id, bool includeActivities);
-    Task<ModuleDto> GetModuleByNameAsync(string name);
-    Task<(Module, ModuleUpdateDto)> GetModuleForPatchAsync(int id);
+    Task<ModuleDto> GetModuleByIdAsync(int courseId, int id, bool includeActivities);
+    Task<ModuleDto> GetModuleByNameAsync(int courseId, string name, bool includeActivities);
+    Task<(Module, ModuleUpdateDto)> GetModuleForPatchAsync(int courseId, int id);
     Task<ModuleDto> CreateModuleAsync(ModuleCreateDto dto);
-    Task UpdateModuleAsync(int id, ModuleUpdateDto dto);
+    Task UpdateModuleAsync(int courseId, int id, ModuleUpdateDto dto);
     Task ApplyModulePatchAsync(Module module, ModuleUpdateDto dto);
 }
