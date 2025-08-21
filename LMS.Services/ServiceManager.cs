ac4db5a403f6653be3c9db0a1b65ab7ded19c05b
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-21 14:42:47 +0200

    Add IModuleService support in ServiceManager
    Updated ServiceManager to include a new dependency on
    IModuleService. Added a private field and property for
    moduleService, and modified the constructor to accept
    Lazy<IModuleService>. Updated IServiceManager interface
    to expose the new ModuleService property.

--- LMS.Services/ServiceManager.cs
+++ LMS.Services/ServiceManager.cs
@@ -1,4 +1,5 @@
 ﻿using Service.Contracts;
+using Services.Contracts;
 
 namespace LMS.Services;
 
@@ -8,6 +9,7 @@ public class ServiceManager : IServiceManager
     private Lazy<IActivityService> activityService;
     private Lazy<IActivityTypeService> activityTypeService;
     private Lazy<IDocumentService> documentService;
+    private Lazy<IModuleService> moduleService;
     public IAuthService AuthService => authService.Value;
 
     public IActivityService ActivityService => activityService.Value;
@@ -16,15 +18,19 @@ public class ServiceManager : IServiceManager
 
     public IDocumentService DocumentService => documentService.Value;
 
+    public IModuleService ModuleService => moduleService.Value;
+
     public ServiceManager(
         Lazy<IAuthService> authService,
         Lazy<IActivityService> activityService,
         Lazy<IActivityTypeService> activityTypeService,
-        Lazy<IDocumentService> documentService)
+        Lazy<IDocumentService> documentService,
+        Lazy<IModuleService> moduleService)
     {
         this.authService = authService;
         this.activityService = activityService;
         this.activityTypeService = activityTypeService;
         this.documentService = documentService;
+        this.moduleService = moduleService;
     }
 }
--- Service.Contracts/IServiceManager.cs
+++ Service.Contracts/IServiceManager.cs
@@ -1,8 +1,11 @@
-﻿namespace Service.Contracts;
+﻿using Services.Contracts;
+
+namespace Service.Contracts;
 public interface IServiceManager
 {
     IAuthService AuthService { get; }
     IActivityService ActivityService { get; }
     IActivityTypeService ActivityTypeService { get; }
     IDocumentService DocumentService { get; }
+    IModuleService ModuleService { get; }
 }
