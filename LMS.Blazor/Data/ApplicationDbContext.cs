ef3faa10c5c8c66e835cb7023dedd761745dee03
Iryna <iryna.chorna@hotmail.com>
2025-08-25 14:29:58 +0200

    Update  ApplicationDbContext i LMS.Blazor

--- LMS.Blazor/Data/ApplicationDbContext.cs
+++ LMS.Blazor/Data/ApplicationDbContext.cs
@@ -11,5 +11,39 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
     {
         base.OnModelCreating(builder);
         builder.Entity<ApplicationUser>().ToTable("ApplicationUser");
+        builder.Entity<ActivityType>()
+                .HasMany(at => at.Activities)
+                .WithOne(a => a.Type)
+                .HasForeignKey(a => a.ActivityTypeId);
+
+        builder.Entity<ApplicationUser>()
+            .HasOne(u => u.Course)
+            .WithMany(c => c.Students)
+            .HasForeignKey(u => u.CourseId)
+            .IsRequired(false);
+
+        builder.Entity<ApplicationUser>()
+            .HasMany(u => u.Courses)
+            .WithMany(c => c.Teachers);
+
+        builder.Entity<Document>()
+            .HasOne(d => d.Course)
+            .WithMany(c => c.Documents)
+            .HasForeignKey(d => d.CourseId);
+
+        builder.Entity<Document>()
+            .HasOne(d => d.Module)
+            .WithMany(m => m.Documents)
+            .HasForeignKey(d => d.ModuleId);
+
+        builder.Entity<Document>()
+            .HasOne(d => d.Activity)
+            .WithMany(a => a.Documents)
+            .HasForeignKey(d => d.ActivityId);
+
+        builder.Entity<Document>()
+            .HasOne(d => d.User)
+            .WithMany(u => u.Documents)
+            .HasForeignKey(d => d.UploadedByUserId);
     }
 }
