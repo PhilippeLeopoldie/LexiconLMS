33fd13e5f96c9422b46c6009df3f488f2b326e63
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-25 17:01:47 +0200

    Refactor CourseNotFoundException parameter name
    Changed constructor parameter from `id` to `courseId` for clarity. Updated error message to accurately reflect the course ID being referenced.

--- Domain.Models/Exceptions/NotFoundException.cs
+++ Domain.Models/Exceptions/NotFoundException.cs
@@ -24,7 +24,7 @@ public class ModuleNotFoundException : NotFoundException
 
 public class CourseNotFoundException : NotFoundException
 {
-    public CourseNotFoundException(int id) : base($"No Course with id: {id}  found!")
+    public CourseNotFoundException(int courseId) : base($"No Course with id: {courseId}  found!")
     {
     }
 }
