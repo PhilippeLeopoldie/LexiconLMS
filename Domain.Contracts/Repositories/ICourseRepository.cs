9fc58054c63ac47ee1551939d910e3866b3f67ff
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-27 21:15:32 +0200

    Add user role filtering to GetAllCoursesAsync method
    Updated ICourseRepository to include an optional parameter
    `UserRole? includeUsers` in the `GetAllCoursesAsync` method
    for filtering courses based on user roles. Modified the
    implementation in CourseRepository accordingly. Added
    necessary using directive for LMS.Shared.Enums in both files.

--- Domain.Contracts/Repositories/ICourseRepository.cs
+++ Domain.Contracts/Repositories/ICourseRepository.cs
@@ -1,11 +1,17 @@
 ﻿using Domain.Models.Entities;
 using LMS.Shared.Common;
+using LMS.Shared.Enums;
 
 namespace Domain.Contracts.Repositories;
 
 public interface ICourseRepository : IRepositoryBase<Course>, IInternalRepositoryBase<Course>
 {
     Task<Course?> GetCourseByIdAsync(int id, bool trackChanges = false);
-    Task<PagedList<Course>> GetAllCoursesAsync(bool includeModules = false, bool includeActivities = false, RequestParams requestParams = null!, bool trackChanges = false);
+    Task<PagedList<Course>> GetAllCoursesAsync(
+        UserRole? includeUsers = null,
+        bool includeModules = false,
+        bool includeActivities = false,
+        RequestParams requestParams = null!,
+        bool trackChanges = false);
     Task<Course?> GetByStudentIdAsync(string studentUserId);
 }
