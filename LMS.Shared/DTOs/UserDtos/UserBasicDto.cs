2961cf6b1a4fdb3683610e26681f6b15969bfd66
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-08-27 21:24:02 +0200

    Enhance DTOs with new properties and ID type changes
    - Added `Ends` property to `CourseDto` and modified `Teacher` to use `UserDto?`.
    - Introduced `Students` property in `CourseDto` for student representation.
    - Changed `Id` type from `string` to `Guid` in `UserBasicDto` and from `int` to `Guid` in `UserDto` for consistent user identification.

--- LMS.Shared/DTOs/UserDtos/UserBasicDto.cs
+++ LMS.Shared/DTOs/UserDtos/UserBasicDto.cs
@@ -8,7 +8,7 @@ namespace LMS.Shared.DTOs.UserDtos;
 
 public record UserBasicDto
 {
-    public string Id { get; set; } = null!;
+    public Guid Id { get; set; } 
     public string? UserName { get; set; }
     public string? Email { get; set; }
 }
