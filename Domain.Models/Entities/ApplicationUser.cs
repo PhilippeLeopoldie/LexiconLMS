f54a2b5a9b3d36505ad5723fa490ee83aa00d7c8
Jörgen Hägglund <jorgenhagglund@netscape.net>
2025-09-02 10:42:30 +0200

    Added FirstName, LastName and calculated FillName to ApplicationUser.

--- Domain.Models/Entities/ApplicationUser.cs
+++ Domain.Models/Entities/ApplicationUser.cs
@@ -4,6 +4,9 @@ namespace Domain.Models.Entities;
 
 public class ApplicationUser : IdentityUser
 {
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? FullName => $"{FirstName} {LastName}".Trim();
     public string? RefreshToken { get; set; }
     public DateTime RefreshTokenExpireTime { get; set; }
 
