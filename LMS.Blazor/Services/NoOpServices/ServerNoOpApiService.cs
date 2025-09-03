966dab3315ce6fbc24f4d90259634daa36b372c2
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-09-03 10:23:30 +0200

    Add PostApiAsync method to ServerNoopApiService
    Updated the `ServerNoopApiService` class to include a new method `PostApiAsync`. This method facilitates handling POST API calls by accepting an `endpoint`, `data`, and an optional `CancellationToken`. It returns a `Task<T?>`, aligning with the existing `CallApiAsync` method's functionality.

--- LMS.Blazor/Services/NoOpServices/ServerNoOpApiService.cs
+++ LMS.Blazor/Services/NoOpServices/ServerNoOpApiService.cs
@@ -5,4 +5,7 @@ namespace LMS.Blazor.Services.NoOpServices;
 public class ServerNoopApiService : IApiService
 {
     public Task<T?> CallApiAsync<T>(string endpoint, CancellationToken cancellationToken = default) => Task.FromResult<T?>(default);
+
+    public Task<T?> PostApiAsync<T>(string endpoint, object data, CancellationToken cancellationToken = default) => Task.FromResult<T?>(default);
+   
 }
