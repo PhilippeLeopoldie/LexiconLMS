026be2564b792e71ade37666949cf01b27b0fadc
Philippe Leopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2025-09-08 14:35:18 +0200

    Fix Swedish grammar in exception messages
    Updated constructor messages in several exception classes for grammatical accuracy and clarity in Swedish. Adjusted date formatting in `ModuleOverlappingException` to ensure only the date part is included.

--- Domain.Models/Exceptions/ConflictException.cs
+++ Domain.Models/Exceptions/ConflictException.cs
@@ -11,31 +11,31 @@ public class ConflictException : Exception
 
 public class ModuleOverlappingException : ConflictException
 {
-    public ModuleOverlappingException(string moduleRange) : base($"Tidsintervallet: {moduleRange} överlappar med ett annat module i denna kurs.")
+    public ModuleOverlappingException(string moduleRange) : base($"Tidsintervallet: {moduleRange} överlappar med en annan module i denna kurs.")
     {
     }
 
     public ModuleOverlappingException(DateTime courseStart, DateTime courseEnd) 
-        : base($"Modulen måste ligga inom kursens datum: {courseStart} - {courseEnd}.")
+        : base($"Modulen måste ligga inom kursens datum: {courseStart.Date} - {courseEnd.Date}.")
     {
     }
 
 }
 
 public class ActivityOverlapException(string range)
-    : ConflictException($"Tidsintervallet: {range} överlappar med en annan aktivitet i detta moment.")
+    : ConflictException($"Tidsintervallet: {range} överlappar med en annan aktivitet i denna module.")
 { }
 
 public class DuplicateStudentInCourseException : ConflictException
 {
-    public DuplicateStudentInCourseException(string studentId, int courseId) : base($"Elev {studentId} är redan registrerad på kursen {courseId}.")
+    public DuplicateStudentInCourseException(string studentId, int courseId) : base($"Eleven {studentId} är redan registrerad på kursen {courseId}.")
     {
     }
 }
 
 public class DuplicateTeacherInCourseException : ConflictException
 {
-    public DuplicateTeacherInCourseException(string teacherId, int courseId) : base($"Lärare {teacherId} är redan registrerad på kursen {courseId}.")
+    public DuplicateTeacherInCourseException(string teacherId, int courseId) : base($"Läraren {teacherId} är redan registrerad på kursen {courseId}.")
     {
     }
 }
